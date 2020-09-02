24682ba29a039c486b676414653f5dfafd4789b3
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-02 17:25:40 +0000

    nothing new, just played with auto compile thing on Intellij

--- src/main/java/ratz/springframework/recipeproject/controllers/IndexController.java
+++ src/main/java/ratz/springframework/recipeproject/controllers/IndexController.java
@@ -8,7 +8,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index"})
     public String getIndexPage() {
-
+        System.out.println("Something to say....");
         return "index";
     }
 
