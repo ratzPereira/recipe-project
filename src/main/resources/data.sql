500ef647fd82cd4e3dd9534934f25a5877be227a
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-03 13:34:06 +0000

    added data.sql to populate category and UOM tables

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT into category (description) VALUES ('American');
+INSERT into category (description) VALUES ('Italian');
+INSERT into category (description) VALUES ('Mexican');
+INSERT into category (description) VALUES ('Fast Food');
+INSERT into UNIT_OF_MEASURE (uom) VALUES ('Teaspoon');
+INSERT into UNIT_OF_MEASURE (uom) VALUES ('Tablespoon');
+INSERT into UNIT_OF_MEASURE (uom) VALUES ('Cup');
+INSERT into UNIT_OF_MEASURE (uom) VALUES ('Pinch');
+INSERT into UNIT_OF_MEASURE (uom) VALUES ('Ounce');
