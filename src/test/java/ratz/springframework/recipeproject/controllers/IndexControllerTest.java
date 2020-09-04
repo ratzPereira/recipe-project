54de284793c409da16c8231edbe9b22d691ea3c4
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-04 17:28:17 +0000

    added argument capture on recipe service test to test if set is returning recipes!

--- src/test/java/ratz/springframework/recipeproject/controllers/IndexControllerTest.java
+++ src/test/java/ratz/springframework/recipeproject/controllers/IndexControllerTest.java
@@ -2,10 +2,15 @@ package ratz.springframework.recipeproject.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+import ratz.springframework.recipeproject.domain.Recipe;
 import ratz.springframework.recipeproject.services.RecipeService;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -32,14 +37,34 @@ class IndexControllerTest {
     @Test
     void getIndexPage() {
 
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1);
+
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        //é para usar em vez do anySet, assim temos a confirmaçao do que vem dentro do set é um recipe
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
         String viewName = indexController.getIndexPage(model);
+
+        //then
         assertEquals("index" , viewName);
         verify(recipeService, times(1)).getRecipes();
 
         //eq means equal, if the first equals recipes
         // on source code -> model.addAttribute("recipes", recipeService.getRecipes());
-        verify(model,times(1)).addAttribute(eq("recipes") , anySet());
+        verify(model,times(1)).addAttribute(eq("recipes") , argumentCaptor.capture());
 
+        //confirmação se vem 2 recipes no set
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2,setInController.size());
 
     }
 }
