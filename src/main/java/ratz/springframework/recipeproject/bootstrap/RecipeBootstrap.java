3183bc15cce249eaddedbc8da092215d9fd2dfb6
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-07 23:41:57 +0000

    display all remaining recipe properties in the view

--- src/main/java/ratz/springframework/recipeproject/bootstrap/RecipeBootstrap.java
+++ src/main/java/ratz/springframework/recipeproject/bootstrap/RecipeBootstrap.java
@@ -155,6 +155,9 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
         guacRecipe.getCategories().add(americanCategory);
         guacRecipe.getCategories().add(mexicanCategory);
+        guacRecipe.setUrl("www.google.com");
+        guacRecipe.setServings(4);
+        guacRecipe.setSource("www.facebook.com");
 
         //add to return list
         recipes.add(guacRecipe);
@@ -209,7 +212,9 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
         tacoRecipe.getCategories().add(americanCategory);
         tacoRecipe.getCategories().add(mexicanCategory);
-
+        tacoRecipe.setUrl("www.google.com");
+        tacoRecipe.setServings(4);
+        tacoRecipe.setSource("www.facebook.com");
 
         recipes.add(tacoRecipe);
         return recipes;
